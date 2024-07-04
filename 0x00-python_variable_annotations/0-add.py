38b55de0613c759667dd68ce2299b97de26f5030
NORTH89 <nordintata0@gmail.com>
2024-07-04 17:57:40 +0100

    type-annotated function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    """Add two floats and return their sum.
+
+    Args:
+        a (float): The first float.
+        b (float): The second float.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
