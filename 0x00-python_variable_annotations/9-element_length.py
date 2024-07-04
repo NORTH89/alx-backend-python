bc204a71251f028b54bf29f78169ac02fd104a20
NORTH89 <nordintata0@gmail.com>
2024-07-04 19:08:29 +0100

    Element length

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """ Element length. """
-
 from typing import List, Tuple
 
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
-    Takes a list of strings and returns a list
-    of tuples containing the string and its length.
+    Takes a list of strings as argument and returns a list of
+    tuples. Each tuple contains a string from the input list and
+    the length of that string.
+
+    Args:
+        lst (List[str]): The list of strings to process.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples containing the
+        strings from the input list and their lengths.
     """
     return [(i, len(i)) for i in lst]
