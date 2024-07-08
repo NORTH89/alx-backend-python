ffc45bacb725200afeddca5ca673a008b849f871
NORTH89 <nordintata0@gmail.com>
2024-07-08 09:31:53 +0100

    write an asynchronous coroutine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ returns the float of the random delay """
+    return random.uniform(0, max_delay)
