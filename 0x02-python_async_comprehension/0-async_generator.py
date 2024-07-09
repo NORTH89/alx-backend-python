360f770ba9d45c80a5e3cafc65a55d08108c7494
NORTH89 <nordintata0@gmail.com>
2024-07-09 11:13:39 +0100

    demonstrates the usage of asynchronous generators

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates the usage of
+asynchronous generators.
+"""
+
+import asyncio
+import random
+
+async def async_generator():
+    """
+    An asynchronous generator that yields
+    a random integer between 0 and 10
+    after a 1-second delay.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
