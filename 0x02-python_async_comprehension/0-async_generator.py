b33e58466d68f96fe1344b043487422ee473cd7a
NORTH89 <nordintata0@gmail.com>
2024-07-09 11:26:45 +0100

    demonstrates the usage of asynchronous generators

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,22 +8,11 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[int, None, None]:
     """
     Returns an asynchronous generator.
     """
-    for _  in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-        
-    
-if __name__ == "__main__":
-    asyncio_generator = __import__('0-async_generator').async_generator
-    
-    async def print_yielded_values():
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-        
-    asyncio.run(print_yielded_values())
