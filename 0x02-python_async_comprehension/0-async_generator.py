3a697d6e4dfce67bcb6fba6b73a36ca56ba23a4d
NORTH89 <nordintata0@gmail.com>
2024-07-09 11:27:38 +0100

    demonstrates the usage of asynchronous generators

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Returns an asynchronous generator.
     """
